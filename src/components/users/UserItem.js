06117b584e41734347a4a75dff04e1a8cc774dd5
Keegan H <keeganslane@gmail.com>
2019-08-30 17:20:28 -0700

    remove constructor

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 
 class UserItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      id: 'id',
-      login: 'mojombo',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-      html_url: 'https://github.com/mojombo'
-    };
-  }
+  state = {
+    id: 'id',
+    login: 'mojombo',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+    html_url: 'https://github.com/mojombo'
+  };
 
   render() {
     return (
@@ -23,7 +20,7 @@ class UserItem extends Component {
 
         <h3>{this.state.login}</h3>
         <div>
-          <a href={this.state.html_url} className="btn btn-dark.btn-sm my-1">
+          <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">
             More
           </a>
         </div>
