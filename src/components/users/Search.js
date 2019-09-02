f5bf150815f421b00f6bd82e378a90955bdc73a2
Keegan H <keeganslane@gmail.com>
2019-09-02 12:21:41 -0700

    update search component state with form

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  state = {
+    text: ''
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
   render() {
     return (
       <div>
         <form className="form">
-          <input type="text" name="text" placeholder="Search Users...." />
+          <input
+            type="text"
+            name="text"
+            placeholder="Search Users...."
+            value={this.state.text}
+            onChange={this.onChange}
+          />
           <input
             type="submit"
             value="Search"
