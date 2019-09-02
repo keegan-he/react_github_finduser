998be9fb2aa00f7eeaecfbf40106bfa740df7e8e
Keegan H <keeganslane@gmail.com>
2019-09-01 21:38:59 -0700

    installed axios / fetch api

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 class App extends Component {
+  async componentDidMount() {
+    const res = await axios.get('https://api.github.com/users');
+    console.log(res.data);
+  }
+
   render() {
     return (
       <div className="App">
