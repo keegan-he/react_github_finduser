9c374d343b409325c837430bf7fe1e19c5f32c83
Keegan H <keeganslane@gmail.com>
2019-09-02 11:41:01 -0700

    prop types added to Users.js

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
-
-
+import PropTypes from 'prop-types';
 
 const Users = ({ users, loading }) => {
-
-if (loading) {
-  return <Spinner />
-} else {
-
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
         {users.map(user => (
@@ -17,15 +14,17 @@ if (loading) {
         ))}
       </div>
     );
+  }
+};
 
-}
-
-
-}
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
 
 const userStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3, 1fr)',
-    gridGap: '1rem'
-}
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem'
+};
 export default Users;
