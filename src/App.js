578145ae5432f00e04cddbb5eadc4f8ef81b35d3
Keegan H <keeganslane@gmail.com>
2019-09-07 20:24:56 -0700

    alert added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   // async componentDidMount() {
@@ -37,6 +39,13 @@ class App extends Component {
   //Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
+  //set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
   render() {
     const { users, loading } = this.state;
 
@@ -44,10 +53,12 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
