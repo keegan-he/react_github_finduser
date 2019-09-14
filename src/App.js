9efa334b7e7c095f7fcf09e1a1fc191aa08cce3d
Keegan H <keeganslane@gmail.com>
2019-09-13 22:11:09 -0700

    create user state

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
@@ -30,12 +31,26 @@ class App extends Component {
   //Search Github users
   searchUsers = async text => {
     this.setState({ loading: true });
+
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
     {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
     this.setState({ users: res.data.items, loading: false });
+  };
+
+  //Get Single Github user
+
+  getUser = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+  {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
   };
 
   //Clear users from state
