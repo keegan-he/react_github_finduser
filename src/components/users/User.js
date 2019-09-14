9a92f8d7040cb4c66df3209cf4ac60b826e7b3f9
Keegan H <keeganslane@gmail.com>
2019-09-13 23:19:19 -0700

    badge data

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -98,6 +98,14 @@ export class User extends Component {
             </ul>
           </div>
         </div>
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers: {followers}</div>
+          <div className="badge badge-success">Followings: {following}</div>
+          <div className="badge badge-danger">
+            Public Repositories: {public_repos}
+          </div>
+          <div className="badge badge-dark">Public Gists: {public_gists}</div>
+        </div>
       </Fragment>
     );
   }
